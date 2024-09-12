2cdae922e05b267dba32a29c9ed023c01c85a611
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-09-11 23:30:48 -0300

    att

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function verificar(){
      var data = new Date()
      var ano  = data.getFullYear()
      var fano = document.getElementById('txtano')
-     var numsStr = document.getElementById('txtano')
      var res  = document.querySelector('div#res')
      if ( fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente !')
