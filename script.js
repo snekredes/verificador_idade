66926cf38f84b92ebea745b894d1d41f69a046d3
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-09-11 23:25:35 -0300

    att

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function verificar(){
      var data = new Date()
      var ano  = data.getFullYear()
      var fano = document.getElementById('txtano')
+     var numsStr = document.getElementById('txtano')
      var res  = document.querySelector('div#res')
      if ( fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente !')
@@ -56,3 +57,4 @@ function verificar(){
     
     
     }
+    
